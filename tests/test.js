958b44f003721b6d2db2325c25c3ab7c5bec0955
Gianglian <cgiang@umass.edu>
2015-03-09 21:10:46 -0400

    Revert "karma setup to run a test"
    This reverts commit e7f801506b543f047b4f88a6031a9b262aedff99.

--- tests/test.js
+++ tests/test.js
@@ -1,56 +1,10 @@
-//var assert = require('chai').assert;
-//var should = require('chai').should;
-//require(['chai']);
+var assert = require('assert');
+var should = require('should');
+var expect = require('expect');
 
-//var chai = require('chai');
-//var expect = chai.expect;
-/*require(['chai'], function() {
-
-});*/
-//var expect = require('chai').expect;
-
-/*describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-2, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});*/
-
-describe('Practice', function() {
-	it('should return false since 2 does not equal 1', function() {
-		assert.equal((1),(1));
-	});
-});
-
-/*
-describe('Art', function() {
-	beforeEach(module('artBoard'));
-	it('should create art and txtArt with 3 fields each', inject(function($service) {
-		var imgArt = {},
-			txtArt = {},
-			ctrl = $service('Art', {$imgArt:imgArt, $txtArt:txtArt});
-		expect(imgArt.length).toBe(3);
-		expect(txtArt.length).toBe(3);
-	}));
-}
-	it('should return the correct fields', inject(function($service)) {
-		var imgArt = [{artist: 'Place Holder', title: 'Washington', img: '../images/600x600.gif'},
-    				  {artist: 'Place Holder', title: 'Washi', img: '../images/300.gif'},
-    				  {artist: 'Place Holder', title: 'Wngton', img: '../images/1000x500.gif'}]
-
-			txtArt = [{artist: 'Place Holdeer', title: 'Abstract Art', txt: 'Placeholder texweofoiebgoegril erough oewri oergh woeirgj ekgbe riv eirg erugh ernek rioeru ier t Placeholder text Placeholder text Placeholder text Placeholder textPlaceholder text Placeholder text Placeholder text Placeholder text'},
-    				  {artist: 'Place Holdeer', title: 'Art of a different sort', txt: 'Placeholder text Placeholder text Placeholder text Placeholder text Placeholder textPlaceholder  regioher wiguh  eriogh eiuotext Placeholder text Placeholder text Placeholder text'},
-    				  {artist: 'Place Holdeer', title: 'Wahingtof', txt: 'Placeholder text Placeholder text Placeholder text Placeholder text Placeholder textPlaceholder text Placeholder text Placeholder text Placeholder text'}]
-
-			ctrl = $service('Art', {$imgArt=imgArt, $txtArt=txtArt});
-	});
-});*/
-/*
 describe('input', function() {
 	before(function() {
-		console.log("input should be abc")
+		console.log("input should be adb")
 	});
 	it('input should be read correctly', function(done) {
 		var input = getInput();
@@ -59,18 +13,11 @@ describe('input', function() {
 });
 
 describe('head to head algorithm', function() {
-			var pieceOne = getPieceOne();
-			var pieceTwo = getPieceTwo();
-			var pieceOneElo = pieceOne.getElo();
-			var pieceTwoElo = pieceTwo.getElo();
 	it('winning piece should gain elo', function(done) {
-			battle(pieceOne, pieceTwo);
-			var pieceOneEloAfter = pieceOne.getElo();
-			var pieceOneEloAfter = pieceOne.getElo();
-
+		var pieceOneElo
 	});
 	it('losing piece should lose', function(done) {
-			battle(pieceOne, pieceTwo);
+
 	});
 });
 
@@ -119,4 +66,16 @@ describe('user log in', function() {
 
 	});
 });
-*/
+
+describe('User', function(){ //taken off stackoverflow, need to revise 
+  describe('#save()', function(){
+    it('should save without error', function(done){
+      var user = new User('Luna');
+      user.save(function(err){
+        if (err) throw err;
+        done();
+      });
+    })
+  })
+})
+
