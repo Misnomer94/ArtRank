9fc25d1d7355443f73943c0d3474bab6e27de878
Senh Wang <senhwang3@gmail.com>
2015-03-23 13:53:47 -0400

    Formatting fixes

--- tests/test.js
+++ tests/test.js
@@ -9,40 +9,40 @@ describe('Art', function() {
 			var ctrl = $controller('artCtrl', ['Art', function(Art){
 				this.imgArt = getImgArt();
 				this.txtArt = getTxtArt();
-                this.audioArt = getAudioArt();
+                		this.audioArt = getAudioArt();
 			}]);
 
 			expect(ctrl.imgArt.length).to.equal(3);
 			expect(ctrl.txtArt.length).to.equal(3);
-            expect(ctrl.audioArt.length).to.equal(3);
+            		expect(ctrl.audioArt.length).to.equal(3);
 		}));
 
 		it('should return the correct fields of imgArt, txtArt, and audioArt', inject(function($controller) {
 			var ctrl = $controller('artCtrl', ['Art', function(Art){
 				this.imgArt = getImgArt();
 				this.txtArt = getTxtArt();
-                this.audioArt = getAudioArt();
+                		this.audioArt = getAudioArt();
 			}]);
 
 			var img = [
 			{artist: 'Place Holder', title: 'Washington', img: '../images/600x600.gif'},
 			{artist: 'Place Holder', title: 'Washi', img: '../images/300.gif'},
 			{artist: 'Place Holder', title: 'Wngton', img: '../images/1000x500.gif'}];
-            {artist: 'Place Holder', title: 'Music', img: '../images/music.jpg'},
-            {artist: 'Place Holder', title: 'Tree', img: '../images/tree.jpg'},
-            {artist: 'Place Holder', title: 'Animals', img: '../images/animals.jpg'},
-            {artist: 'Place Holder', title: 'Up There', img: '../images/way-up-there.jpg'},
-            {artist: 'Place Holder', title: 'Bricks', img: '../images/brick-wallpaper.jpg'},
+            		{artist: 'Place Holder', title: 'Music', img: '../images/music.jpg'},
+            		{artist: 'Place Holder', title: 'Tree', img: '../images/tree.jpg'},
+            		{artist: 'Place Holder', title: 'Animals', img: '../images/animals.jpg'},
+            		{artist: 'Place Holder', title: 'Up There', img: '../images/way-up-there.jpg'},
+            		{artist: 'Place Holder', title: 'Bricks', img: '../images/brick-wallpaper.jpg'},
 
 			var txt = [
 			{artist: 'Place Holdeer', title: 'Abstract Art', txt: 'Placeholder texweofoiebgoegril erough oewri oergh woeirgj ekgbe riv eirg erugh ernek rioeru ier t Placeholder text Placeholder text Placeholder text Placeholder textPlaceholder text Placeholder text Placeholder text Placeholder text'},
 			{artist: 'Place Holdeer', title: 'Art of a different sort', txt: 'Placeholder text Placeholder text Placeholder text Placeholder text Placeholder textPlaceholder  regioher wiguh  eriogh eiuotext Placeholder text Placeholder text Placeholder text'},
 			{artist: 'Place Holdeer', title: 'Wahingtof', txt: 'Placeholder text Placeholder text Placeholder text Placeholder text Placeholder textPlaceholder text Placeholder text Placeholder text Placeholder text'},
             
-            var audio = [
-            {artist: 'Place Holder', title: 'Washington', audio: '../images/preview.mp3'},
-            {artist: 'Place Holder', title: 'Washi', audio: '../images/preview (1).mp3'},
-            {artist: 'Place Holder', title: 'Wngton', audio: '../images/preview (2).mp3'}];
+            		var audio = [
+            		{artist: 'Place Holder', title: 'Washington', audio: '../images/preview.mp3'},
+            		{artist: 'Place Holder', title: 'Washi', audio: '../images/preview (1).mp3'},
+            		{artist: 'Place Holder', title: 'Wngton', audio: '../images/preview (2).mp3'}];
 
 			for(var i = 0; i < ctrl.imgArt.length; i++){ 
 				expect(ctrl.imgArt[i].artist).to.equal(img[i].artist);
@@ -53,7 +53,7 @@ describe('Art', function() {
 				expect(ctrl.txtArt[i].title).to.equal(txt[i].title);
 				expect(ctrl.txtArt[i].txt).to.equal(txt[i].txt);
                 
-                expect(ctrl.audioArt[i].artist).to.equal(audio[i].artist);
+                		expect(ctrl.audioArt[i].artist).to.equal(audio[i].artist);
 				expect(ctrl.audioArt[i].title).to.equal(audio[i].title);
 				expect(ctrl.audioArt[i].txt).to.equal(audio[i].audio);
 			}
