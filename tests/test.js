da21b938e8eaa183696dc6b320ff41a66d93fdd4
SenhW <senhwang3@gmail.com>
2015-03-23 17:51:44 -0400

    Bug fixes

--- tests/test.js
+++ tests/test.js
@@ -27,17 +27,17 @@ describe('Art', function() {
 			var img = [
 			{artist: 'Place Holder', title: 'Washington', img: '../images/600x600.gif'},
 			{artist: 'Place Holder', title: 'Washi', img: '../images/300.gif'},
-			{artist: 'Place Holder', title: 'Wngton', img: '../images/1000x500.gif'}];
+			{artist: 'Place Holder', title: 'Wngton', img: '../images/1000x500.gif'},
 			{artist: 'Place Holder', title: 'Music', img: '../images/music.jpg'},
 			{artist: 'Place Holder', title: 'Tree', img: '../images/tree.jpg'},
 			{artist: 'Place Holder', title: 'Animals', img: '../images/animals.jpg'},
 			{artist: 'Place Holder', title: 'Up There', img: '../images/way-up-there.jpg'},
-			{artist: 'Place Holder', title: 'Bricks', img: '../images/brick-wallpaper.jpg'},
+			{artist: 'Place Holder', title: 'Bricks', img: '../images/brick-wallpaper.jpg'}];
 
 			var txt = [
 			{artist: 'Place Holdeer', title: 'Abstract Art', txt: 'Placeholder texweofoiebgoegril erough oewri oergh woeirgj ekgbe riv eirg erugh ernek rioeru ier t Placeholder text Placeholder text Placeholder text Placeholder textPlaceholder text Placeholder text Placeholder text Placeholder text'},
 			{artist: 'Place Holdeer', title: 'Art of a different sort', txt: 'Placeholder text Placeholder text Placeholder text Placeholder text Placeholder textPlaceholder  regioher wiguh  eriogh eiuotext Placeholder text Placeholder text Placeholder text'},
-			{artist: 'Place Holdeer', title: 'Wahingtof', txt: 'Placeholder text Placeholder text Placeholder text Placeholder text Placeholder textPlaceholder text Placeholder text Placeholder text Placeholder text'},
+			{artist: 'Place Holdeer', title: 'Wahingtof', txt: 'Placeholder text Placeholder text Placeholder text Placeholder text Placeholder textPlaceholder text Placeholder text Placeholder text Placeholder text'}];
             
 			var audio = [
 			{artist: 'Place Holder', title: 'Washington', audio: '../images/preview.mp3'},
